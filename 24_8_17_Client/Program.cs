25e204e638b224d2c983f3036b3b2eebd11c2700
Matthew Peterson <matt2694@edu.eal.dk>
2017-08-24 09:44:54 +0200

    Add project files.

---
+++ 24_8_17_Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_8_17_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+
+        private void Run()
+        {
+            TcpClient client = new TcpClient("localhost", 5000);
+            StreamReader sr = new StreamReader(client.GetStream());
+            StreamWriter sw = new StreamWriter(client.GetStream());
+
+            try
+            {
+                string data = Console.ReadLine();
+                sw.WriteLine(data);
+                sw.Flush();
+                while (data != null && data != "")
+                {
+                    data = sr.ReadLine();
+                    Console.WriteLine(data);
+
+                    data = Console.ReadLine();
+                    sw.WriteLine(data);
+                    sw.Flush();
+                }
+                client.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
